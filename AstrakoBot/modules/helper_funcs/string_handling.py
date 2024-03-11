e1880bda391b8b240616225ed5e671128c6ccb95
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2024-03-11 04:55:14 +0400

    update string_handling.py

--- AstrakoBot/modules/helper_funcs/string_handling.py
+++ AstrakoBot/modules/helper_funcs/string_handling.py
@@ -47,14 +47,14 @@ def _selective_escape(to_parse: str) -> str:
     return to_parse
 
 
-# This is a fun one.
+# Bu əyləncəlidir.
 def _calc_emoji_offset(to_calc) -> int:
-    # Get all emoji in text.
+    # Bütün emojiləri mətndə əldə edin.
     emoticons = emoji.get_emoji_regexp().finditer(to_calc)
-    # Check the utf16 length of the emoji to determine the offset it caused.
-    # Normal, 1 character emoji don't affect; hence sub 1.
-    # special, eg with two emoji characters (eg face, and skin col) will have length 2, so by subbing one we
-    # know we'll get one extra offset,
+    # Emojinin səbəb olduğu ofseti müəyyən etmək üçün onun utf16 uzunluğunu yoxlayın. 
+    # Normal, 1 simvol emoji təsir etmir; buna görə də 1-ci alt. 
+    # xüsusi, məsələn, iki emoji simvolu olan (məsələn, üz və dəri rəngi) uzunluğu 2 olacaq, ona görə də birini əlavə etməklə biz 
+    # bilirik ki, bir əlavə ofset alacağıq,
     return sum(len(e.group(0).encode("utf-16-le")) // 2 - 1 for e in emoticons)
 
 
@@ -62,15 +62,14 @@ def markdown_parser(
     txt: str, entities: Dict[MessageEntity, str] = None, offset: int = 0
 ) -> str:
     """
-    Parse a string, escaping all invalid markdown entities.
+    Bütün etibarsız markdown obyektlərindən qaçaraq sətri təhlil edin.
 
-    Escapes URL's so as to avoid URL mangling.
-    Re-adds any telegram code entities obtained from the entities object.
+    URL-lərin dəyişdirilməsinin qarşısını almaq üçün URL-lərdən qaçır. Müəssisələr obyektindən əldə edilən istənilən teleqram kodu obyektlərini yenidən əlavə edir.
 
-    :param txt: text to parse
-    :param entities: dict of message entities in text
-    :param offset: message offset - command and notename length
-    :return: valid markdown string
+    :param txt: təhlil etmək üçün mətn 
+    :param entities: mətndəki mesaj obyektlərinin diktəsi
+    :param ofset: mesaj ofset - komanda və qeyd adı uzunluğu 
+    :return: etibarlı işarələmə sətri
     """
     if not entities:
         entities = {}
@@ -79,42 +78,43 @@ def markdown_parser(
 
     prev = 0
     res = ""
-    # Loop over all message entities, and:
-    # reinsert code
-    # escape free-standing urls
+   # Bütün mesaj obyektləri üzərində dövr edin və: 
+   # kodu yenidən daxil edin 
+   # müstəqil URL-lərdən qaçın
     for ent, ent_text in entities.items():
         if ent.offset < -offset:
             continue
 
-        start = ent.offset + offset  # start of entity
-        end = ent.offset + offset + ent.length - 1  # end of entity
+        start = ent.offset + offset  
+        # qurumun başlanğıcı
+        end = ent.offset + offset + ent.length - 1  # qurumun sonu
 
-        # we only care about code, url, text links
+        # Biz yalnız kod, url, mətn bağlantıları ilə maraqlanırıq
         if ent.type in ("code", "url", "text_link"):
-            # count emoji to switch counter
+            # sayğacı dəyişmək üçün emoji sayın
             count = _calc_emoji_offset(txt[:start])
             start -= count
             end -= count
 
-            # URL handling -> do not escape if in [](), escape otherwise.
+            # URL-lərin idarə edilməsi -> []() varsa qaçmayın, əks halda qaçın.
             if ent.type == "url":
                 if any(
                     match.start(1) <= start and end <= match.end(1)
                     for match in LINK_REGEX.finditer(txt)
                 ):
                     continue
-                # else, check the escapes between the prev and last and forcefully escape the url to avoid mangling
+                # əks halda, əvvəlki və sonuncu arasındakı qaçışları yoxlayın və sındırmamaq üçün url-dən zorla qaçın
                 else:
-                    # TODO: investigate possible offset bug when lots of emoji are present
+                    # TODO: çoxlu emoji mövcud olduqda mümkün ofset səhvini araşdırın
                     res += _selective_escape(txt[prev:start] or "") + escape_markdown(
                         ent_text
                     )
 
-            # code handling
+            # kodla işləmə
             elif ent.type == "code":
                 res += _selective_escape(txt[prev:start]) + "`" + ent_text + "`"
 
-            # handle markdown/html links
+            # markdown/html bağlantılarını idarə edin
             elif ent.type == "text_link":
                 res += _selective_escape(txt[prev:start]) + "[{}]({})".format(
                     ent_text, ent.url
@@ -122,13 +122,15 @@ def markdown_parser(
 
             end += 1
 
-        # anything else
+        # başqa bir şey
         else:
             continue
 
         prev = end
 
-    res += _selective_escape(txt[prev:])  # add the rest of the text
+    res += _selective_escape(txt[prev:])  
+   
+    # mətnin qalan hissəsini əlavə edin
     return res
 
 
@@ -140,20 +142,21 @@ def button_markdown_parser(
     note_data = ""
     buttons = []
     for match in BTN_URL_REGEX.finditer(markdown_note):
-        # Check if btnurl is escaped
+      
+        # btnurl-in qaçıb olmadığını yoxlayın
         n_escapes = 0
         to_check = match.start(1) - 1
         while to_check > 0 and markdown_note[to_check] == "\\":
             n_escapes += 1
             to_check -= 1
 
-        # if even, not escaped -> create button
+        # əgər hətta, xilas deyil -> yaratmaq düyməsi
         if n_escapes % 2 == 0:
-            # create a thruple with button label, url, and newline status
+            # düymə etiketi, url və yeni sətir statusu ilə thruple yaradın
             buttons.append((match.group(2), match.group(3), bool(match.group(4))))
             note_data += markdown_note[prev : match.start(1)]
             prev = match.end(1)
-        # if odd, escaped -> move along
+        # qəribədirsə, qaçdı -> hərəkət et
         else:
             note_data += markdown_note[prev:to_check]
             prev = match.start(1) - 1
@@ -220,9 +223,9 @@ def split_quotes(text: str) -> List:
     else:
         return text.split(None, 1)
 
-    # 1 to avoid starting quote, and counter is exclusive so avoids ending
+    # Başlanğıc sitatın qarşısını almaq üçün 1 və sayğac eksklüzivdir, buna görə də bitmənin qarşısını alır
     key = remove_escapes(text[1:counter].strip())
-    # index will be in range, or `else` would have been executed and returned
+    # indeks diapazonda olacaq və ya `başqa` yerinə yetirilib qaytarılacaqdı
     rest = text[counter + 1 :].strip()
     if not key:
         key = text[0] + text[0]
@@ -258,7 +261,7 @@ def extract_time(message, time_val):
         unit = time_val[-1]
         time_num = time_val[:-1]  # type: str
         if not time_num.isdigit():
-            message.reply_text("Invalid time amount specified.")
+            message.reply_text("Yanlış vaxt məbləği göstərildi.")
             return ""
 
         if unit == "m":
@@ -273,7 +276,7 @@ def extract_time(message, time_val):
         return bantime
     else:
         message.reply_text(
-            "Invalid time type specified. Expected m,h, or d, got: {}".format(
+            "Yanlış vaxt növü göstərildi. Gözlənilən m,h və ya d, alındı: {}".format(
                 time_val[-1]
             )
         )
