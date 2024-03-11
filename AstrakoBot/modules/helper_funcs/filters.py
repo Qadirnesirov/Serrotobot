1ca24ac3f12f3942b1c45115f9d469d53d1ab96d
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2024-03-11 05:07:29 +0400

    update filters.py

--- AstrakoBot/modules/helper_funcs/filters.py
+++ AstrakoBot/modules/helper_funcs/filters.py
@@ -1,4 +1,4 @@
-from AstrakoBot import DEV_USERS, SUDO_USERS, SUPPORT_USERS
+from SerroToBot import DEV_USERS, SUDO_USERS, SUPPORT_USERS
 from telegram import Message
 from telegram.ext import MessageFilter
 
